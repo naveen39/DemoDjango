9c25d6287317d742223a4d5afaada783ffa6f75f
naveen39 <naveensfdc9@gmail.com>
2018-12-20 09:54:32 -0800

    Add files via upload

--- dempproject/settings.py
+++ dempproject/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath('staticfiles')))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
