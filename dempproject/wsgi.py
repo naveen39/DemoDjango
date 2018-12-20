e0c48e143003db94862f42e8a642ad23d04141be
naveen39 <naveensfdc9@gmail.com>
2018-12-20 09:59:38 -0800

    Add files via upload

--- dempproject/wsgi.py
+++ dempproject/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dempproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demodjango1.settings')
 
 application = get_wsgi_application()
 
