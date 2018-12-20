dabb45a2867ec8bbc7d6e211a94e229d58ebb1e3
naveen39 <naveensfdc9@gmail.com>
2018-12-20 09:08:51 -0800

    Add files via upload

--- dempproject/settings.py
+++ dempproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
